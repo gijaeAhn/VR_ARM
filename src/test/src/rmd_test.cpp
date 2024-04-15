92640ad14759f71f29910f596699480c5e7574d3
gijaeAhn <dksrlwo@gmail.com>
2024-04-15 12:02:18 +0900

    zmq_fresh

--- src/test/src/rmd_test.cpp
+++ src/test/src/rmd_test.cpp
@@ -14,7 +14,6 @@
 #include <algorithm>
 #include <cstdint>
 
-utilities::memory::SHM<float> RMD_TORQUE_TEST_SHM(RMD_MOTOR_KEY,RMD_MOTOR_SIZE);
 
 
 
@@ -36,7 +35,6 @@ int main(int argc,char* argv[])
 
         printf("Torque : %f \n", rmd_torque_test_buffer[0]);
 
-        RMD_TORQUE_TEST_SHM.SHM_WRITE(rmd_torque_test_buffer);
         
         sleep(1);
 
@@ -61,7 +59,6 @@ int main(int argc,char* argv[])
 
 void signalHandler(int signum) {
     printf("Interrupt siganl (%d) recived\n", signum);
-    RMD_TORQUE_TEST_SHM.SHM_FREE();
     exit(EXIT_SUCCESS);
 }
 
