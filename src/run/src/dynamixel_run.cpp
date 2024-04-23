ec0a25a7094d898d933bfeb24ddace901a4af189
gijaeAhn <dksrlwo@gmail.com>
2024-04-23 11:03:56 +0900

    DYNAMIXEL

--- src/run/src/dynamixel_run.cpp
+++ src/run/src/dynamixel_run.cpp
@@ -38,10 +38,16 @@
 #define ADDR_PRO_GOAL_POSITION          596
 #define ADDR_PRO_PRESENT_POSITION       611
 
+#define ADDR_CURRENT_GOAL               102
+#define ADDR_TORUQE_ENABEL              64
+#define ADDR_PRESENT_POSITION           132
+
 // Data Byte Length
 #define LEN_PRO_LED_RED                 1
 #define LEN_PRO_GOAL_POSITION           4
 #define LEN_PRO_PRESENT_POSITION        4
+#define LEN_CURRENT_GOAL                4
+#define LEN_PRESENT_POSITION            4
 
 // Protocol version
 #define PROTOCOL_VERSION                2.0                 // See which protocol version is used in the Dynamixel
@@ -56,6 +62,9 @@
 #define DXL_MINIMUM_POSITION_VALUE      -150000             // Dynamixel will rotate between this value
 #define DXL_MAXIMUM_POSITION_VALUE      150000              // and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
 #define DXL_MOVING_STATUS_THRESHOLD     20                  // Dynamixel moving status threshold
+
+#define TORQUE_CONSTANT                 2.12
+#define CURRENT_UNIT                    2.69
 
 #define ESC_ASCII_VALUE                 0x1b
 
@@ -143,9 +152,12 @@ int main(int argc, char* argv[]){
     //May be not a float
     std::array<float,DYNAMIXEL_MOTOR_SIZE> angBuffer;
     std::array<float,DYNAMIXEL_MOTOR_SIZE> currentBuffer;
+    std::array<uint16_t,DYNAMIXEL_MOTOR_SIZE> unitCurrentBuffer;
+    std::array<uint16_t,DYNAMIXEL_MOTOR_SIZE> unitAngBuffer;
     sumBuffer.fill(0);
     angBuffer.fill(0);
     currentBuffer.fill(0);
+    unitAngBuffer.fill(0);
 
     // Open port
     if (portHandler->openPort())
@@ -194,7 +206,7 @@ int main(int argc, char* argv[]){
 
 
     for(uint8_t index = 0 ; index < DYNAMIXEL_MOTOR_SIZE; index++){
-      dxl_addparam_result = groupBulkRead.addParam(DXL1_ID + index, ADDR_PRO_PRESENT_POSITION, LEN_PRO_PRESENT_POSITION);
+      dxl_addparam_result = groupBulkRead.addParam(DXL1_ID + index, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION);
       if (dxl_addparam_result != true)
       {
         fprintf(stderr, "[ID:%03d] grouBulkRead addparam failed", DXL1_ID + index);
@@ -213,95 +225,73 @@ int main(int argc, char* argv[]){
         zmq::recv_result_t result = dyna_torque_subscriber.recv(torque_sub_message,zmq::recv_flags::none);
         std::copy(sumBuffer.begin(),sumBuffer.end(),static_cast<float*>(torque_sub_message.data()));
 
-        //Torque Constant( Torque / Current ) ~= 2.12
+        //Torque Constant( Torque / Current ) ~= 2.12 // Suppose it's linear
         //Protocol Unit = 2.69mA
         //Current Limit = 0 ~ 2047
         //Current Range == -Current Limit ~ Current Limit
-        for(uint8_t index)
+        for(uint8_t index = 0; index < DYNAMIXEL_MOTOR_SIZE; index++){
+          unitCurrentBuffer[index] = static_cast<uint16_t>(((sumBuffer[index] / TORQUE_CONSTANT) * 1000) / CURRENT_UNIT);
+          torque_goal[index][0] = DXL_LOBYTE(DXL_LOWORD(unitCurrentBuffer[index]));
+          torque_goal[index][1] = DXL_HIBYTE(DXL_LOWORD(unitCurrentBuffer[index]));
+          torque_goal[index][2] = DXL_LOBYTE(DXL_HIWORD(unitCurrentBuffer[index]));
+          torque_goal[index][3] = DXL_HIBYTE(DXL_HIWORD(unitCurrentBuffer[index]));
+        }
 
       
 
-
-
-
-
-
-
-        // Add parameter storage for Dynamixel#1 goal position
-        dxl_addparam_result = groupBulkWrite.addParam(DXL1_ID, ADDR_PRO_GOAL_POSITION, LEN_PRO_GOAL_POSITION, param_goal_position);
-        if (dxl_addparam_result != true)
-        {
-          fprintf(stderr, "[ID:%03d] groupBulkWrite addparam failed", DXL1_ID);
-          return 0;
+        for(uint8_t index = 0; index < DYNAMIXEL_MOTOR_SIZE; index++){
+          // Add parameter storage for Dynamixel#1 goal position
+          dxl_addparam_result = groupBulkWrite.addParam(DXL1_ID + index, ADDR_CURRENT_GOAL, LEN_CURRENT_GOAL, torque_goal[index]);
+          if (dxl_addparam_result != true)
+          {
+            fprintf(stderr, "[ID:%03d] groupBulkWrite addparam failed", DXL1_ID);
+            return 0;
+          }
         }
-
-        // Add parameter storage for Dynamixel#2 LED value
-        dxl_addparam_result = groupBulkWrite.addParam(DXL2_ID, ADDR_PRO_LED_RED, LEN_PRO_LED_RED, &dxl_led_value[index]);
-        if (dxl_addparam_result != true)
-        {
-          fprintf(stderr, "[ID:%03d] groupBulkWrite addparam failed", DXL2_ID);
-          return 0;
-        }
-
-        // Bulkwrite goal position and LED value
         dxl_comm_result = groupBulkWrite.txPacket();
         if (dxl_comm_result != COMM_SUCCESS) printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
 
         // Clear bulkwrite parameter storage
         groupBulkWrite.clearParam();
+        //TX End
 
-        do
+
+        // Bulkread present position and LED status
+        dxl_comm_result = groupBulkRead.txRxPacket();
+        if (dxl_comm_result != COMM_SUCCESS)
         {
-          // Bulkread present position and LED status
-          dxl_comm_result = groupBulkRead.txRxPacket();
-          if (dxl_comm_result != COMM_SUCCESS)
-          {
-            printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
-          }
-          else if (groupBulkRead.getError(DXL1_ID, &dxl_error))
-          {
-            printf("[ID:%03d] %s\n", DXL1_ID, packetHandler->getRxPacketError(dxl_error));
-          }
-          else if (groupBulkRead.getError(DXL2_ID, &dxl_error))
-          {
-            printf("[ID:%03d] %s\n", DXL2_ID, packetHandler->getRxPacketError(dxl_error));
-          }
-
-          // Check if groupbulkread data of Dynamixel#1 is available
-          dxl_getdata_result = groupBulkRead.isAvailable(DXL1_ID, ADDR_PRO_PRESENT_POSITION, LEN_PRO_PRESENT_POSITION);
+          printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+        }
+        else if (groupBulkRead.getError(DXL1_ID, &dxl_error))
+        {
+          printf("[ID:%03d] %s\n", DXL1_ID, packetHandler->getRxPacketError(dxl_error));
+        }
+        else if (groupBulkRead.getError(DXL2_ID, &dxl_error))
+        {
+          printf("[ID:%03d] %s\n", DXL2_ID, packetHandler->getRxPacketError(dxl_error));
+        }
+        // Check if groupbulkread data of Dynamixel#1 is available
+        for(uint8_t index = 0; index < DYNAMIXEL_MOTOR_SIZE ; index ++){
+          dxl_getdata_result = groupBulkRead.isAvailable(DXL1_ID + index, ADDR_PRO_PRESENT_POSITION, LEN_PRO_PRESENT_POSITION);
           if (dxl_getdata_result != true)
           {
-            fprintf(stderr, "[ID:%03d] groupBulkRead getdata failed", DXL1_ID);
+            fprintf(stderr, "[ID:%03d] groupBulkRead getdata failed", DXL1_ID + index);
             return 0;
           }
-
-          // Check if groupbulkread data of Dynamixel#2 is available
-          dxl_getdata_result = groupBulkRead.isAvailable(DXL2_ID, ADDR_PRO_LED_RED, LEN_PRO_LED_RED);
-          if (dxl_getdata_result != true)
-          {
-            fprintf(stderr, "[ID:%03d] groupBulkRead getdata failed", DXL2_ID);
-            return 0;
-          }
-
-          // Get present position value
-          dxl1_present_position = groupBulkRead.getData(DXL1_ID, ADDR_PRO_PRESENT_POSITION, LEN_PRO_PRESENT_POSITION);
-
-          // Get LED value
-          dxl2_led_value_read = groupBulkRead.getData(DXL2_ID, ADDR_PRO_LED_RED, LEN_PRO_LED_RED);
-
-          printf("[ID:%03d] Present Position : %d \t [ID:%03d] LED Value: %d\n", DXL1_ID, dxl1_present_position, DXL2_ID, dxl2_led_value_read);
-
-        }while(abs(dxl_goal_position[index] - dxl1_present_position) > DXL_MOVING_STATUS_THRESHOLD);
-
-        // Change goal position
-        if (index == 0)
-        {
-          index = 1;
         }
-        else
-        {
-          index = 0;
+        
+        for(uint8_t index =0 ; index < DYNAMIXEL_MOTOR_SIZE; index++){
+          unitAngBuffer[index] = groupBulkRead.getData(DXL1_ID + index, ADDR_PRESENT_POSITION,LEN_PRESENT_POSITION);
         }
+
+        printf("[ID:%03d] Present Position : %d \t [ID:%03d] LED Value: %d\n", DXL1_ID, dxl1_present_position, DXL2_ID, dxl2_led_value_read);
+
+
+
+        //ZMQ Sending position
+        
+
+
       }
 
       // Disable Dynamixel#1 Torque
